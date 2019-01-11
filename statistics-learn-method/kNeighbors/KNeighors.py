e11974fe8cf1f8d1b81c6c2a57bcbd3606795f3a
xiaorrancs <xiaoranone@gmail.com>
2019-01-11 09:33:34 +0800

    add:note decision tree

---
+++ statistics-learn-method/kNeighbors/KNeighors.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+# Author: xiaoran
+# Gmail : xiaoranone@gmail.com
+# CSDN  : https://blog.csdn.net/fool_ran
+# Time  : 2018-01-05 
+
+class KNeighbors(object):
+	def __init__(self, n_neighbors=5, algorithm="auto", p=2):
+		pass
+	def predict(self, X):
+		pass
+	def _perdict_ont(self, X):
+		pass
+	
